0d8d43a31da825c91a4768915064a4b4e130e678
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2025-08-16 07:54:00 +0000

    Update parlelingua.cr

--- sources/parlelingua.cr
+++ sources/parlelingua.cr
@@ -1,10 +1,122 @@
 ## Function calls
-def bokette(phrase);                              sleep(1.5); puts phrase; end
-def cette(x, y);                             print "Cette #{x} es #{y}. "; end
-def bokecette(x, y);             sleep(1.5); print "Cette #{x} es #{y}. "; end
-def maisette(x, y);                    puts "Mais cette #{x} es ne #{y}."; end
-def bokemaisette(x, y);    sleep(1.5); puts "Mais cette #{x} es ne #{y}."; end
-def sinon(x, y, z);                 puts "Sinon #{x} es ne #{y} ou #{z}."; end
-def bokesinon(x, y, z); sleep(1.5); puts "Sinon #{x} es ne #{y} ou #{z}."; end
+def bokette(phrase)
+  sleep(1.5)
+  
+  puts phrase
+end
+
+def cette(x, y)
+  if x == ""
+    puts "X is not provided."
+    
+    abort
+  end
+  
+  if y == ""
+    puts "X is not provided."
+    
+    abort
+  end
+
+  print "Cette #{x} es #{y}. "
+end
+
+def bokecette(x, y)
+  if x == ""
+    puts "X is not provided."
+    
+    abort
+  end
+  
+  if y == ""
+    puts "X is not provided."
+    
+    abort
+  end
+
+  sleep(1.5)
+
+  print "Cette #{x} es #{y}. "
+end
+
+def maisette(x, y)
+  if x == ""
+    puts "X is not provided."
+    
+    abort
+  end
+  
+  if y == ""
+    puts "X is not provided."
+    
+    abort
+  end
+
+  puts "Mais cette #{x} es ne #{y}."
+end
+
+def bokemaisette(x, y)
+  if x == ""
+    puts "X is not provided."
+    
+    abort
+  end
+  
+  if y == ""
+    puts "X is not provided."
+    
+    abort
+  end
+
+  sleep(1.5)
+
+  puts "Mais cette #{x} es ne #{y}."
+end
+
+def sinon(x, y, z)
+  if x == ""
+    puts "X is not provided."
+    
+    abort
+  end
+  
+  if y == ""
+    puts "X is not provided."
+    
+    abort
+  end
+
+  if z == ""
+    puts "X is not provided."
+    
+    abort
+  end
+
+  puts "Sinon #{x} es ne #{y} ou #{z}."
+end
+
+def bokesinon(x, y, z)
+  if x == ""
+    puts "X is not provided."
+    
+    abort
+  end
+  
+  if y == ""
+    puts "X is not provided."
+    
+    abort
+  end
+
+  if z == ""
+    puts "X is not provided."
+    
+    abort
+  end
+
+  sleep(1.5)
+
+  puts "Sinon #{x} es ne #{y} ou #{z}."
+end
 
 cette("pomme", "rouge"); bokemaisette("carot", "rouge"); sinon("pomme", "legume", "viande")
